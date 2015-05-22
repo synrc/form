2284d4f52348031119a31988a68f6725860b7fbb
Zatolokin Pavel <pavel.zatolokin@privatbank.ua>
2015-05-22 18:23:34 +0300

    add id to button

--- src/forms.erl
+++ src/forms.erl
@@ -152,6 +152,6 @@ new(Document,Object) ->
         % buttons
 
         #panel{id=forpreload,class=buttons,body= lists:foldr(fun(#but{}=But,Acc) ->
-        [#link{class=But#but.class,postback=But#but.postback, body=But#but.title,
+        [#link{id=But#but.id, class=But#but.class, postback=But#but.postback, body=But#but.title,
                source=[wf:atom([S,Name])||S<-But#but.sources]}|Acc] end,[],Buttons)}
     ]}.
