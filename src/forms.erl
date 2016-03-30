65ca5e70b1e456e372a36302c3b0dc1625957bae
Zatolokin Pavel <pavel.zatolokin@privatbank.ua>
2016-03-30 17:28:46 +0300

    change validation pay

--- src/forms.erl
+++ src/forms.erl
@@ -117,7 +117,7 @@ fields(Document, Object) ->
                                                       value=wf:to_list(element(X#field.pos,Object)) },
                                               #panel{ class=pt10,body= [ deposits:translate({?MODULE, warning}), wf:to_binary(X#field.min), " ", X#field.curr ] } ];
                                    pay -> #panel{body=[#input{ id=wf:atom([X#field.name,Name]), pattern="[0-9]*",
-                                                               validation=wf:f("Validation.money(e, ~w, ~w, '~s')",[X#field.min,X#field.max, deposits:translate({?MODULE, error})]),
+                                                               validation=wf:f("Validation.pay(e, ~w, '~s')",[X#field.min,deposits:translate({?MODULE, error})]),
                                                                onkeypress=wf:f("return fieldsFilter(event, ~w, '~w');",[X#field.length,X#field.type]), onkeyup="beautiful_numbers(event);",
                                                                value=wf:to_list(element(X#field.pos,Object)) }, <<" ">>, #span{body=X#field.curr}]};
                                    combo -> case length(Options) of
