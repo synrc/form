604ac81ddd22e2fd9e93d5b98d5c599e094a86d2
Namdak Tonpa <maxim@synrc.com>
2019-11-12 18:29:58 +0200

    4.11.4

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FORM.Mixfile do
   def project do
     [
       app: :form,
-      version: "4.11.3",
+      version: "4.11.4",
       description: "FORM Business X-Forms",
       package: package(),
       deps: deps()
