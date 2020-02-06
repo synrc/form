f18f324b4ac8e3b2fec9b760739fe7fb53dc80f2
Ruslan Moroz <r.moroz@nodeart.io>
2020-02-06 13:01:45 +0200

    added id_rules to formReg record

--- include/formReg.hrl
+++ include/formReg.hrl
@@ -1,6 +1,6 @@
 -ifndef(FORM_REG).
 -define(FORM_REG, true).
 
--record(formReg, {id,name,horizontal,vertical,instance,proc}).
+-record(formReg, {id,name,horizontal,vertical,instance,proc,id_rules}).
 
 -endif.
