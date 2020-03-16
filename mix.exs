db7410805ead2025881c8c433d9b4c21e048e8b7
Oleksandr Naumov <oleksandr.naumov91@gmail.com>
2020-03-16 16:37:33 +0200

    5.3.1

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FORM.Mixfile do
   def project do
     [
       app: :form,
-      version: "5.1.10",
+      version: "5.3.1",
       description: "FORM Business X-Forms",
       package: package(),
       deps: deps()
