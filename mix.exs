83947d9855c455161044b724ead301f32fbe097f
SunRiseGG <SunRiseGC4@gmail.com>
2020-08-06 15:20:30 +0300

    Version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FORM.Mixfile do
   def project do
     [
       app: :form,
-      version: "5.8.1",
+      version: "5.8.3",
       description: "FORM Business X-Forms",
       package: package(),
       deps: deps()
@@ -24,7 +24,7 @@ defmodule FORM.Mixfile do
   def deps do
     [
       {:ex_doc, "~> 0.11", only: :dev},
-      {:nitro, "~> 5.8.0"}
+      {:nitro, "~> 5.8.4"}
     ]
   end
 end
