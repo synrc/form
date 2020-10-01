35b2f148584553ac54fdb76f7bd4a5c6b70a5f5d
SunRiseGC <SunRiseGC4@gmail.com>
2020-10-01 19:18:18 +0300

    Version update

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FORM.Mixfile do
   def project do
     [
       app: :form,
-      version: "5.8.5",
+      version: "5.8.6",
       description: "FORM Business X-Forms",
       package: package(),
       deps: deps()
