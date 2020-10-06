99ff325d5f39a314010a23c07fc634d042597257
SunRiseGC <SunRiseGC4@gmail.com>
2020-10-06 16:07:20 +0300

    Added the ability to add multiple values

--- include/meta.hrl
+++ include/meta.hrl
@@ -10,7 +10,7 @@
 -record(sel,        { id=[], name=[], title=[], postback=[] }).
 -record(field,      { id=[], raw=[], sec=1, name=[], pos=[], title=[], layout=[], visible=true, disabled=false,
                       vector=false, index=[], bind=[], format="~w", curr=[], postfun=[], desc=[], wide=normal,
-                      type=binary, etc=[], labelClass=label, fieldClass=field, boxClass=box, form = [],
+                      type=binary, etc=[], labelClass=label, fieldClass=field, boxClass=box, form = [], multiple=false,
                       required=false, module=[], access=[], tooltips=[], options=[], min=0, distinct=false,
                       max=1000000, length=10, postback=[], onchange=[], validation=[], hidden=false, default=[] }).
 
