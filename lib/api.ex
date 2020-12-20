a2202890891086f4d9dba0a115e9de8ae1ea4474
Namdak Tonpa <maxim@synrc.com>
2020-12-20 02:28:19 +0200

    5.12.1

--- lib/api.ex
+++ lib/api.ex
@@ -1,10 +1,11 @@
-defmodule CRM.Forms.API do
+defmodule FORM.Combo.Sample do
   # TODO: write document in man folder for form.n2o.dev
   require FORM
+  require NITRO
   def doc(), do: {:api,1,2}
   def id(), do: 1
   def new(name, obj, options), do: FORM.document()
-  def proto(msg), do: CRM.Forms.Combo.proto(msg)
+  def proto(msg), do: NITRO.proto(msg)
   def index(), do: [:name, :id]
   def item({:api,x,y}), do: :nitro.jse("#{x} #{y}")
   def view_value({:api,x,y}), do: :io_lib.format('~p ~p',[x,y])
