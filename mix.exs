5c64e8b729e7e323875416f608aaed50c359ecbe
5HT <maxim@synrc.com>
2022-06-23 00:47:49 +0300

    7.6.2

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule FORM.Mixfile do
   def project do
     [
       app: :form,
-      version: "7.6.0",
+      version: "7.6.2",
       description: "FORM Business X-Forms",
       package: package(),
       deps: deps()
